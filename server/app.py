1a52554c835a659d01d2c93aee17b3dd98143f1c
Lemachipascal-dev <lemachijo421@gmail.com>
2024-10-02 15:56:19 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -3,3 +3,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+
+
